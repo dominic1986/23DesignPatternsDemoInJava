005d556195baf6fb111e2851c6bc40916ead3d5f
Dominic Deng <124354176@qq.com>
2017-01-06 07:14:01 +0800

    Add composite pattern code

---
+++ src/main/java/structural/composite/advanced/Ellipse.java
@@ -0,0 +1,13 @@
+package structural.composite.advanced;
+/**
+ * leaf
+ * @author Administrator
+ *
+ */
+class Ellipse implements Graphic {
+
+    //Prints the graphic.
+    public void print() {
+        System.out.println("Ellipse");
+    }
+}
---
+++ src/main/java/structural/composite/advanced/Graphic.java
@@ -0,0 +1,7 @@
+package structural.composite.advanced;
+/** "Component" */
+interface Graphic {
+
+    //Prints the graphic.
+    public void print();
+}
